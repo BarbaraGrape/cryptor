f19265a9db3a9432b7a759a7c17480ea6cb7cca3
Darkclainer <Darkclainer@gmail.com>
2015-10-18 15:34:45 +0300

    Test if file have correct dos header

--- cryptor/main.cpp
+++ cryptor/main.cpp
@@ -3,12 +3,14 @@
 #include <fstream>
 #include <stdexcept>
 
+#include <windows.h>
+
 typedef unsigned char uchar;
 
 void open_file(std::ifstream& i_file)
 {
 	std::string s_file = "";
-	s_file = "C:/test.txt"; //s_file = "C:/cryptor.exe";//std::cin >> s_file;
+	s_file = "C:/cryptor.exe"; //s_file = "C:/cryptor.exe";//std::cin >> s_file;
 
 	i_file.open(s_file, std::ios::binary);
 	if (!i_file)
@@ -23,18 +25,27 @@ int file_size(std::ifstream& i_file)
 }
 
 int main()
+try
 {
 	std::ifstream i_file;
 	open_file(i_file);
 
 	int fs = file_size(i_file);
-
-	uchar* buffer = new uchar[fs];
+	
+	uchar* buffer = new uchar[fs]; 
 	i_file.read(reinterpret_cast<char*>(buffer), fs);
 
-	std::cout << reinterpret_cast<char*>(buffer) << std::endl;
+	IMAGE_DOS_HEADER* dos_h = reinterpret_cast<IMAGE_DOS_HEADER*>(buffer);
+	if (dos_h->e_magic != IMAGE_DOS_SIGNATURE) // first test is this file true PE?
+		throw std::runtime_error("This file is not PE");
 
+	std::cout << "OK!\n";
 	std::cin.get();
-
 	delete[] buffer;
+}
+catch (std::exception& e)
+{
+	std::cout << "Exception: " << e.what() << std::endl;
+	std::cin.get();
+	return 1;
 }
