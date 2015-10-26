db3cecb91cec9a26e7a7ff2a7d30b8dc6aad32df
BarbaraGrape <Barbara_Grape@me.com>
2015-10-26 23:52:56 +0300

    Rebased function version 0.1

--- cryptor/crypt.cpp
+++ cryptor/crypt.cpp
@@ -2,12 +2,37 @@
 
 #include "crypt.h"
 #include <stdint.h>
+#include <Windows.h>
 
 void __stdcall crypt_chunk(uint8_t* chunk, int size, uint8_t mask)
 {
 	for (uint8_t* p = chunk; p < (chunk+size); p++)
 		//*p = 0x42;
 		*p ^= mask;
+}
+void __stdcall rebase(BYTE* ptable, uint32_t size, int diff, int base)
+{
+	BYTE *pend = ptable + size;
+	while ( ptable < pend )
+	{
+		IMAGE_BASE_RELOCATION* rel = reinterpret_cast<IMAGE_BASE_RELOCATION*>(ptable);
+		for (WORD* p = reinterpret_cast<WORD*>(ptable + sizeof(IMAGE_BASE_RELOCATION)); p <  reinterpret_cast<WORD*>(ptable + rel->SizeOfBlock); p++)
+		{
+			int type = (*p & 0xf000) >> 12;
+			int location = (*p & 0x0fff) + rel->VirtualAddress;
+			switch(type)
+			{
+			case IMAGE_REL_BASED_ABSOLUTE:
+				break;
+			case IMAGE_REL_BASED_HIGHLOW:
+				*reinterpret_cast<WORD*>(base+location) += diff;
+				break;
+			default:
+				break;
+			}
+		}
+		ptable += rel->SizeOfBlock;
+	}
 }
 __declspec(naked) void __stdcall new_entry_point(void)
 {
