26be57d475566547fce4d90d404468df8a57d4ad
Darkclainer <Darkclainer@gmail.com>
2015-12-17 21:00:27 +0300

    Code in new section. It's should be work, but not. I don't knew why. (Barbara)

--- cryptor/main.cpp
+++ cryptor/main.cpp
@@ -21,9 +21,13 @@ static const uint32_t XOR_MASK = 242;
 	старой релок секции. Саму .релок секцию перезаполним на наш новый код а потом запихнём инфу,
 	которая там была.
 */
-inline int align(int begin, int alignment)
+int align(int begin, int alignment)
 {
-	begin += alignment - (begin % alignment);
+	int off = begin % alignment;
+	if (!off)
+		return begin;
+	else
+		return begin + alignment - off;
 }
 inline uint32_t get_fn_ptr(void* fn)
 {
@@ -78,6 +82,60 @@ IMAGE_SECTION_HEADER* get_reloc_section(IMAGE_NT_HEADERS* nt_h)
 
 	return found ? sec : NULL;
 }
+IMAGE_SECTION_HEADER* create_new_code_section(IMAGE_NT_HEADERS* nt_h, uint8_t* buffer)
+{
+	// todo: situation when not availabe enough space for new section	
+	/* what we do:
+		inc number of section in nt header
+		acquire suitable pointer for IMAGE_SECTION_HEADER
+		set for new section name and characteristic		
+	*/
+	void* p =  nt_h + 1;
+	IMAGE_SECTION_HEADER* new_sec = static_cast<IMAGE_SECTION_HEADER*>(p) + nt_h->FileHeader.NumberOfSections;
+	std::memset(new_sec, 0, sizeof(IMAGE_SECTION_HEADER));
+	
+	BYTE name[IMAGE_SIZEOF_SHORT_NAME] = ".code";
+	name[IMAGE_SIZEOF_SHORT_NAME-1] = 0;
+	memcpy(new_sec->Name, name, IMAGE_SIZEOF_SHORT_NAME); // Name
+	
+	new_sec->Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_EXECUTE; 
+	
+	nt_h->FileHeader.NumberOfSections++; // new section added
+	return new_sec;
+}
+// return section, that apear last in memory
+IMAGE_SECTION_HEADER* get_last_section(IMAGE_NT_HEADERS* nt_h) 
+{
+	if (!nt_h)
+		return NULL;
+
+	void* p = nt_h + 1;
+	IMAGE_SECTION_HEADER* last = static_cast<IMAGE_SECTION_HEADER*>(p);
+	IMAGE_SECTION_HEADER* current = last+1;
+
+	for (int i = 1; i < nt_h->FileHeader.NumberOfSections; ++i)
+	{
+		if (last->VirtualAddress < current->VirtualAddress)
+			last = current;
+		current++;
+	}
+	return last;
+}
+void set_flag_for_sections(IMAGE_NT_HEADERS* nt_h, uint32_t set_flags, uint32_t clear_flags)
+{
+	if (!nt_h)
+		return;
+
+	void* p = nt_h + 1;
+	IMAGE_SECTION_HEADER* sec = static_cast<IMAGE_SECTION_HEADER*>(p);
+
+	for (int i = 0; i < nt_h->FileHeader.NumberOfSections; ++i)
+	{
+		sec->Characteristics |= set_flags;
+		sec->Characteristics &= ~(clear_flags);
+		sec++;
+	}
+}
 
 int main(void)
 try
@@ -112,76 +170,66 @@ try
 
 	IMAGE_SECTION_HEADER* reloc_sec = get_reloc_section(nt_h);
 
-
-	//
 #ifdef _DEBUG
 #error Can't calcute correct difference of functions address
 #endif
-	int stub_size = get_fn_ptr(end_point) - get_fn_ptr(crypt_chunk);
-
-	crypt_chunk(buffer.data() + code_sec->PointerToRawData, code_sec->SizeOfRawData, XOR_MASK);
+	// crypt code section;
+	crypt_chunk(&buffer[code_sec->PointerToRawData], code_sec->SizeOfRawData, XOR_MASK);
 	/* 
 		rva - relative addres in mem
 		pf - pointer in file
 		va - pointer in mem + ImageBase
 	*/
 
-	int end_code_pf = code_sec->PointerToRawData + code_sec->Misc.VirtualSize; // where we can put our code
-	int free_space_size = code_sec->SizeOfRawData - code_sec->Misc.VirtualSize; // space that we can use
-	int end_code_rva = code_sec->VirtualAddress + code_sec->Misc.VirtualSize;
-	uint32_t stub_pf = end_code_pf;
-	
-	stub_pf = align(stub_pf, 16);
+	// allocate memory for stub+data
+	int my_code_size = get_fn_ptr(end_point) - get_fn_ptr(crypt_chunk);
+	int full_size = align(my_code_size + sizeof(Some_data), opt_h->FileAlignment);
+	std::vector<uint8_t> stub(full_size, 0);
+	std::memcpy(stub.data(), crypt_chunk, my_code_size);
 
-	int gap = stub_pf - end_code_pf;
-	if (gap + stub_size + sizeof(Some_data) > free_space_size)
-		throw std::runtime_error("No space to write stub");
-	int stub_rva = end_code_rva + gap;
-
-	std::memset(buffer.data() + end_code_pf, 0, free_space_size);
-	std::memcpy(buffer.data() + stub_pf, crypt_chunk, stub_size);
+	//calculate variable related to new section
+	IMAGE_SECTION_HEADER* new_sec = create_new_code_section(nt_h, buffer.data());
+	IMAGE_SECTION_HEADER* last_sec = get_last_section(nt_h);
+	new_sec->VirtualAddress		= align(last_sec->VirtualAddress + last_sec->Misc.VirtualSize, opt_h->SectionAlignment);
+	new_sec->Misc.VirtualSize	= full_size;
+	new_sec->SizeOfRawData		= align(full_size, opt_h->FileAlignment);
+	new_sec->PointerToRawData	= align(fs, opt_h->FileAlignment);
 	
-	//set stub correct params
 	int nep_offset	= get_fn_ptr(new_entry_point) - get_fn_ptr(crypt_chunk);
-	uint32_t nep_pf = stub_pf + nep_offset;
-	int nep_rva		= stub_rva+ nep_offset;
-	int nep_va		= stub_rva+ nep_offset + opt_h->ImageBase + 5; // with offset.. sub, call and ect see asm code
+	int nep_rva		= new_sec->VirtualAddress + nep_offset;
 
-	int code_begin_va = opt_h->ImageBase + code_sec->VirtualAddress;
+	int image_base = opt_h->ImageBase;
+	Some_data inf;
+	inf.nep_va		= image_base + nep_rva;
+	inf.cb_va		= image_base + code_sec->VirtualAddress;
+	inf.xor_mask	= XOR_MASK;
+	inf.code_vsize	= code_sec->Misc.VirtualSize;
+	inf.oep_va		= image_base + opt_h->AddressOfEntryPoint;
+	if (reloc_sec)
+	{
+		inf.br_va		= image_base + reloc_sec->VirtualAddress;
+		inf.reloc_vsize = reloc_sec->Misc.VirtualSize;
+	}
+	else
+		inf.reloc_vsize = 0;
+	memcpy(&stub[my_code_size], &inf, sizeof(Some_data));
 
-	int oep_va = opt_h->AddressOfEntryPoint + opt_h->ImageBase;
-	
+
 	//set new entry point
-	opt_h->AddressOfEntryPoint	 = nep_rva;
-	code_sec->Characteristics		|= 0xE0000060;
-	code_sec->Misc.VirtualSize		+= gap + stub_size;
+	//opt_h->AddressOfEntryPoint = nep_rva;
+	opt_h->SizeOfCode += my_code_size;
 
-	// relocation
 	IMAGE_DATA_DIRECTORY* data_reloc = &opt_h->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
 	data_reloc->Size = 0;
 	data_reloc->VirtualAddress = NULL;
 
-	Some_data inform;
-	inform.nep_va = nep_va;
-	inform.cb_va = code_begin_va;
-	inform.xor_mask = XOR_MASK;
-	inform.code_vsize = code_sec->Misc.VirtualSize;
-	inform.oep_va = oep_va;
-	if (reloc_sec)
-	{
-		inform.br_va = opt_h->ImageBase + reloc_sec->VirtualAddress;
-		inform.reloc_vsize = reloc_sec->Misc.VirtualSize;
-	}
-	else
-		inform.reloc_vsize = 0;
-
-	IMAGE_SECTION_HEADER* new_sec = reloc_sec + 1; // in idea it clean section
-	new_sec->PointerToRawData = reloc_sec->PointerToRawData + reloc_sec->SizeOfRawData; //after ather .reloc section
-	
+	set_flag_for_sections(nt_h, IMAGE_SCN_MEM_WRITE, IMAGE_SCN_MEM_DISCARDABLE);
+	//reloc_sec->Characteristics &= ~(IMAGE_SCN_MEM_DISCARDABLE);
 
 	//open file for output 
 	std::ofstream o_file(cryptorNewFilename, std::ofstream::binary | std::ofstream::out | std::ofstream::trunc);
 	o_file.write(reinterpret_cast<char*>(buffer.data()), fs);
+	o_file.write(reinterpret_cast<char*>(stub.data()), full_size);
 
 	std::cout << "OK!\n";
 
